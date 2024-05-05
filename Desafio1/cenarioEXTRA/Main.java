de21f736fdc007dcd936320c93b1cbcceb35e738
Luana-And <167131086+Luana-And@users.noreply.github.com>
2024-05-05 20:18:32 -0300

    Add pasta na dev

---
+++ Desafio1/cenarioEXTRA/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class EmoticonCounter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite a mensagem:");
+        String mensagem = scanner.nextLine();
+
+        // Implementação do contador de emoticons vem depois
+    }
+}
