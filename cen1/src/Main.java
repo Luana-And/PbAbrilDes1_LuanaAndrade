ededb413063763b4ea532fd25fdc5cf9669c7b7f
Luana-And <167131086+Luana-And@users.noreply.github.com>
2024-05-06 16:31:15 -0300

    Update Main.java

--- cen1/src/Main.java
+++ cen1/src/Main.java
@@ -1,65 +1,41 @@
+import java.util.ArrayList;
+import java.util.List;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
         Person lily = new Person("Lily");
         lily.spouse(new Person("Wilhelm"));
-
         // Patriarch and matriarch
-        Person opa = new Person("Opa");
+        Person opa = new Person("Opa", lily);
         opa.spouse(new Person("Oma"));
-
         // Children of Oma and Opa
-        Person reinhold = new Person("Reinhold");
-        reinhold.spouse(new Person("Sonia"));
-        opa.addChild(reinhold);
-
-        Person wilma = new Person("Wilma");
-        wilma.spouse(new Person("Rodolfo"));
-        opa.addChild(wilma);
-
-        Person sigrid = new Person("Sigrid");
-        sigrid.spouse(new Person("Peter"));
-        opa.addChild(sigrid);
-
+        Person reinhold = new Person("Reinhold", opa);
+        Person wilma = new Person("Wilma", opa);
+        Person sigrid = new Person("Sigrid", opa);
         // Children and spouse of Reinhold
-        Person christian = new Person("Christian");
-        reinhold.addChild(christian);
-
-        Person gabrielle = new Person("Gabrielle");
-        reinhold.addChild(gabrielle);
-
-        Person sabine = new Person("Sabine");
-        reinhold.addChild(sabine);
-
+        reinhold.spouse(new Person("Sonia"));
+        Person christian = new Person("Christian", reinhold);
+        Person gabrielle = new Person("Gabrielle", reinhold);
+        Person sabine = new Person("Sabine", reinhold);
         // Children and spouse of Wilma
-        Person ricardo = new Person("Ricardo");
-        wilma.addChild(ricardo);
-
-        Person rodrigo = new Person("Rodrigo");
-        wilma.addChild(rodrigo);
-
+        wilma.spouse(new Person("Rodolfo"));
+        Person ricardo = new Person("Ricardo", wilma);
+        Person rodrigo = new Person("Rodrigo", wilma);
         // Ricardo and spouse
         ricardo.spouse(new Person("Debora"));
-
         // Children and spouse of Christian
-        Person oscar = new Person("Oscar");
-        christian.addChild(oscar);
-
-        Person lorena = new Person("Lorena");
-        christian.addChild(lorena);
         christian.spouse(new Person("Monica"));
-
+        Person oscar = new Person("Oscar", christian);
+        Person lorena = new Person("Lorena", christian);
         // Children and spouse of Sigrid
-        Person martin = new Person("Martin");
-        sigrid.addChild(martin);
+        sigrid.spouse(new Person("Peter"));
+        Person martin = new Person("Martin", sigrid);
+        Person thomas = new Person("Thomas", sigrid);
+        Person claudia = new Person("Claudia", sigrid);
         martin.spouse(new Person("Carla"));
         martin.addChild(new Person("Nicolas"));
-
-        Person thomas = new Person("Thomas");
-        sigrid.addChild(thomas);
-
-        Person claudia = new Person("Claudia");
-        sigrid.addChild(claudia);
 
         lily.printFamilyTree(0);
         opa.printFamilyTree(1);
