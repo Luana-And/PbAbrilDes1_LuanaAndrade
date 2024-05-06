16861d3fcc2970d77dfb387f024a8341b63ab1a3
Luana-And <167131086+Luana-And@users.noreply.github.com>
2024-05-06 16:52:30 -0300

    add package

---
+++ cen2/src/model/Adult.java
@@ -0,0 +1,21 @@
+package model;
+
+public class Adult {
+
+    private final String nome;
+    private final int age;
+
+    public Adult(String nome, int age){
+        this.nome = nome;
+        this.age = age;
+    }
+
+    public String getName() {
+        return this.nome;
+    }
+
+    public int getAge() {
+        return this.getAge();
+    }
+
+}
